2c3212525242128f0b5f574b195465ce3fd590be
Xiyuan729 <2477739653@qq.com>
2026-06-16 19:44:14 +0800

    debug: add verbose CI output for diagnosis

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -179,7 +179,7 @@ for round in $(seq 1 "$REPEAT"); do
     ctest \
         --test-dir "$BUILD_DIR" \
         -j"$JOBS" \
-        --output-on-failure \
+        --output-on-failure -V \
         "${CTEST_LABEL_ARGS[@]}"
     RC=$?
     set -e
